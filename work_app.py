26ee05c33f162dac5fcf9539e872fd9ab1fb9445
itayhadash <62889824+itayhadash@users.noreply.github.com>
2025-05-02 09:12:50 +0000

    update

--- work_app.py
+++ work_app.py
@@ -8,9 +8,15 @@ from streamlit.runtime.scriptrunner import add_script_run_ctx
 import financedatabase as fd
 
 st.title("Finance Analysis App")
+@st.cache_data
+def load_tic():
+    return list(pd.Series(list(set(fd.Equities().select().index))))
 
-sp500_tickers = list(pd.Series(list(set(fd.Equities().select().index).union(set(fd.ETFs().select().index)))).dropna().values)
-select_name = st.selectbox("select S&P 500 stock",sp500_tickers)
+
+sp500_tickers = load_tic()
+
+select_name = st.selectbox("select stock",sp500_tickers)
+st.write(fd.Equities().select().query("symbol==@select_name").name.values[0])
 select_period = st.selectbox('select_period',['1mo','1d','1w','1y'])
 select_interval = st.selectbox('select_interval',['1mo','1d','1w','1y'])
 execute = st.button("execute")
@@ -30,7 +36,8 @@ if execute:
     col4.metric("End", str((round(historical_data.Open[historical_data.shape[0]-1]))) + "$")
 
     con = st.container(border=True)
-    con.line_chart(historical_data, x="Date", y="Close")
+    con.line_chart(historical_data, x="Date", y="Close",x_label="Date", y_label='Close price')
+
 else:
     None
 
